818cb8e2efb411c5484bfb783150fd902e49d916
kevinaragn <kevinmartinaragon@gmail.com>
2022-09-22 22:16:43 -0300

    Ejemplo Proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ContenidoComponent } from './components/contenido/contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MenuComponent,
+    ContenidoComponent,
   ],
   imports: [
     BrowserModule
